ff856fb6e2c4a8682375279cadd9776c544b77da
Tomas <horsekit1982@gmail.com>
2014-10-02 21:49:27 +0800

    login and logout

--- api/__init__.py
+++ api/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.ext.restful import Api
 from flask.ext.mongoengine import MongoEngine
 from flask.ext.script import Manager
+from flask.ext.login import LoginManager
 
 app = Flask(__name__)
 api = Api(app)
@@ -13,5 +14,16 @@ db = MongoEngine(app)
 
 from api.users import UserAPI
 from api.login import LoginAPI
+
+login_manager = LoginManager()
+login_manager.setup_app(app)
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return 'unauthorized'
+
+@login_manager.user_loader
+def load_user(userid):
+    return model.User.objects.with_id(userid)
 
 import model
--- api/login.py
+++ api/login.py
@@ -1,15 +1,32 @@
 #-*- coding:utf-8 -*-
 from flask import request
 from api import api
+from itsdangerous import Signer
 from model import User
 from flask.ext.restful import Resource, reqparse
-#Login.py
+from flask.ext.login import (login_user, logout_user,
+        current_user, login_required)
+
 class LoginAPI(Resource):
     def __init__(self):
-        pass
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('account', type=str, help=u'No account exist')
+
     def put(self):
-        return request.get_json(force=True)
+        data = request.get_json(force=True)
+        data['sign_pwd'] = Signer(data['pwd']).sign('pwd')
+        try:
+            user = User.objects(**data).first()
+        except:
+            return 'Search DB Error'
+        if user:
+            login_user(user)
+            return user.to_json()
+        return  'Account or PasswordError'
+
+    @login_required
     def delete(self):
-        return request.get_json(force=True)
+        logout_user()
+        return 'Logout Success!'
 
 api.add_resource(LoginAPI, '/loginout/', endpoint='loginout')
